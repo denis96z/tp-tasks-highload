41dc7a5c38181a0deb72aa4f11b7eba4537e3f76
denis96z <zinoviev.denis@yandex.ru>
2019-03-25 17:49:50 +0300

    prometheus metrics

--- sample-web-app/src/main/java/ru/mail/park/samplewebapp/controller/HelloController.java
+++ sample-web-app/src/main/java/ru/mail/park/samplewebapp/controller/HelloController.java
@@ -2,12 +2,14 @@ package ru.mail.park.samplewebapp.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.micrometer.core.annotation.Timed;
 
 @RestController
 @RequestMapping("/message")
 public class HelloController {
 
     @GetMapping
+    @Timed
     public ResponseEntity<String> getMessage() throws InterruptedException {
         Thread.sleep(500);
         return ResponseEntity.ok("Hello world!");
